3bd2d1c94cbd5d71a9b01f7d242d24ea74927db4
paperbd <heather.hb@gmail.com>
2013-04-20 09:00:26 -0700

    Completed initial design of project, called Treader.

---
+++ project/application.py
@@ -0,0 +1,41 @@
+/*
+Treader - A GUI-based MP3 Tag Reader
+Author: Heather Hoaglund-Biron
+*/
+
+from multiprocessing import Process
+import Tkinter
+import dbhelper.DatabaseHelper as DatabaseHelper
+import tagreader.TagReader as TagReader
+
+class Application(Frame):
+    def __init__(self, master=None):
+        """
+        Initializes required objects in the main application, including a 
+        TagReader object and a DatabaseHelper object.
+
+        """
+        Frame.__init__(self, master)
+        self.treader = TagReader()
+        self.dbhelper = DatabaseHelper()
+
+    def createWidgets(self):
+        """
+        Creates the widgets used in the application.
+
+        """
+
+    def importTags(self):
+        """
+        Uses the treader to read tags from .mp3 files and the dbhelper to 
+        insert them in the database. Runs in a separate process.
+
+        """
+
+    def getTags(self):
+        """
+        Uses the dbhelper to query the database for tags and then returns them 
+        in a way that the GUI can use them. Runs in a separate process.
+        
+        """
+
---
+++ project/dbhelper.py
@@ -0,0 +1,32 @@
+/*
+Database Helper
+Author: Heather Hoaglund-Biron
+*/
+
+import sqlite3
+
+class DatabaseHelper:
+    """
+    A DatabaseHelper reads and writes MP3 metadata to an SQLite database.
+
+    """
+
+    def __init__(self):
+        """
+        Initializes the MP3 metadata database.
+
+        """
+
+    def write(self, data):
+        """
+        Takes the given data and writes it to the database.
+
+        """
+
+    def read(self, query):
+        """
+        Reads the information given in the query, grabs the specified data from 
+        the database, and returns it.
+
+        """
+
